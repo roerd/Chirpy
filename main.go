d19ba0502972ec9fcd8d9c700a32678f248348af
Rörd Hinrichsen <roerdhh@gmail.com>
2026-02-23 22:13:43 +0100

    Serve files from the project dir

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
